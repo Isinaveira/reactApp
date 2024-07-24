12bbcf9dfef7f19c11ee2a0be0cbfae6955502de
Isi naveira <isinaveira@gmail.com>
2024-07-24 12:20:40 -0400

    CartItem created functions

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,30 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((acc, item) => {return acc + parseFloat(item.cost.replace('$', '')) * item.quantity } , 0) //eliminamos el dolar y lo pasamos a float 
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.replace('$', '')) * item.quantity;
   };
 
   return (
